238dd43d769c94a18928afd9df17b6a4e6477afb
Shobhit Kumar Sharma <sharmashobhit1000@gmail.com>
2025-03-04 19:57:01 +0530

    New changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,22 @@ const Message = require("./models/message");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://portfolio-shobhit.vercel.app",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://portfolio-shobhit.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
